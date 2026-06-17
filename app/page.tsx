4ad4cee4ed48b4052d1c6e11a73200398063455c
Miki <osamu@kagebunshinai.com>
2026-06-17 21:59:28 +0900

    feat: page.tsxをDESIGN.md §5構成順に組み込み

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
 import { Hero } from './components/Hero'
-import { Problem } from './components/Problem'
 import { Story } from './components/Story'
+import { Problem } from './components/Problem'
 import { Strength } from './components/Strength'
-import { CTA } from './components/CTA'
+import { Menu } from './components/Menu'
+import { Voices } from './components/Voices'
+import { Staff } from './components/Staff'
 import { Access } from './components/Access'
+import { CTA } from './components/CTA'
+import { FAQ } from './components/FAQ'
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <Problem />
       <Story />
+      <Problem />
       <Strength />
-      <CTA />
+      <Menu />
+      <Voices />
+      <Staff />
       <Access />
+      <CTA />
+      <FAQ />
     </main>
   )
 }
